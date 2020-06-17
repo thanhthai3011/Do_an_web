bf92738d9d984172f33f6b77a67f82daf5ac314a
ThanhDev <letrungthanh101>
2020-06-18 02:41:29 +0700

    continuteAdmin

---
+++ Do_an_web/Controllers/TempController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Do_an_web.Models;
+
+namespace Do_an_web.Controllers
+{
+    public class TempController : Controller
+    {
+        CourseEntities _db = new CourseEntities();
+        // GET: Temp
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getMenu()
+        {
+            var v = from t in _db.menus
+                    where t.hide == true
+                    orderby t.order ascending
+                    select t;
+                return PartialView(v.ToList());
+        }
+    }
+}
